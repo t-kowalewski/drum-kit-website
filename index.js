2442e5e825186d7352d6540f5d13be5ee264338d
t-kowalewski <wkowalewski@outlook.com>
2019-01-28 21:59:53 +0100

    added eventListener v 1 + css addon (button pressed)

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var buttonLen = document.querySelectorAll(".drum").length;
 
 for (var index = 0; index < buttonLen; index++) {
-    document.querySelectorAll(".drum")[index].addEventListener("click", handleClick); //we passing function without ()
+    // document.querySelectorAll(".drum")[index].addEventListener("click", handleClick); //we pass function without ()
+    document.querySelectorAll(".drum")[index].addEventListener("click", function () { // or use anon function
+        this.style.color = "white"; // or -> this.classList.add("button-pressed");
+        // console.log(this);
+    });
 }
 
-function handleClick() {
-    alert("I got clicked");
-}
+
+
+// function handleClick() {
+//     alert("I got clicked");
+// }
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
